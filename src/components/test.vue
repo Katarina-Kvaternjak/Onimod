7aa7d82a59fbb66f258a4b7ffb4ad7a9763b2ba5
Katarina Kvaternjak <80330761+Katarina-Kvaternjak@users.noreply.github.com>
2021-11-02 21:02:54 +0100

    Update

---
+++ src/components/test.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <h1>Onimod</h1>
+    <var-button loading type="warning" loading-type="wave" color="green">Loading Status</var-button>
+    <var-loading type="rect" style="color: green" />
+    <var-loading type="cube"  style="color: green"/>
+</div>
+</template>
+<style scoped>
+    div{
+        margin:2%;
+    }
+</style>
